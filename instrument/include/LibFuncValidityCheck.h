eec1fdb262c6e5e56e6e9aa2ed6e4693db5fdd47
Akash Kothari <akashk4@illinois.edu>
2019-08-08 13:02:31 -0400

    Create LibFuncValidityCheck.h

---
+++ instrument/include/LibFuncValidityCheck.h
@@ -0,0 +1,15 @@
+
+#ifndef LIB_FUNC_VALIDITY_CHECK_H__
+#define LIB_FUNC_VALIDITY_CHECK_H__
+
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Attributes.h"
+
+bool IsValidLibMemoryOperation(const FunctionType &FTy, LibFunc F,
+															 const DataLayout &DL);
+
+bool CalleeIsTerminatesProgram(Function *Callee);
+
+#endif // LIB_FUNC_VALIDITY_CHECK_H__
